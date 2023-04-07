0383760a02acf5d3fd1c642266136c86af65e175
Mark Otuya <markotuya0@gmail.com>
2023-04-07 08:17:14 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+TextField,
+Button,
+Grid,
+Container,
+Typography,
+FormControl,
+Select,
+MenuItem,
+} from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const PatientReferralForm = () => {
+const [formData, setFormData] = useState({
+patientName: "",
+age: "",
+gender: "",
+reasonForReferral: "",
+referredBy: "",
+contactNumber: "",
+email: "",
+});
 
-export default App;
+const handleChange = (event) => {
+const { name, value } = event.target;
+setFormData((prevFormData) => ({
+...prevFormData,
+[name]: value,
+}));
+};
+
+const handleSubmit = (event) => {
+event.preventDefault();
+// Handle form submission logic here
+console.log("Form Data:", formData);
+};
+
+return (
+<Container maxWidth="sm">
+<Typography variant="h4" align="center" gutterBottom>
+Patient Referral Form
+</Typography>
+<form onSubmit={handleSubmit}>
+<Grid container spacing={2}>
+<Grid item xs={12}>
+<TextField
+           fullWidth
+           variant="outlined"
+           label="Patient Name"
+           name="patientName"
+           value={formData.patientName}
+           onChange={handleChange}
+         />
+</Grid>
+<Grid item xs={12}>
+<TextField
+           fullWidth
+           variant="outlined"
+           label="Age"
+           name="age"
+           value={formData.age}
+           onChange={handleChange}
+         />
+</Grid>
+<Grid item xs={12}>
+<FormControl fullWidth variant="outlined">
+<Select
+             label="Gender"
+             name="gender"
+             value={formData.gender}
+             onChange={handleChange}
+           >
+<MenuItem value="male">Male</MenuItem>
+<MenuItem value="female">Female</MenuItem>
+<MenuItem value="other">Other</MenuItem>
+</Select>
+</FormControl>
+</Grid>
+<Grid item xs={12}>
+<TextField
+           fullWidth
+           variant="outlined"
+           label="Reason for Referral"
+           name="reasonForReferral"
+           value={formData.reasonForReferral}
+           onChange={handleChange}
+           multiline
+           rows={4}
+         />
+</Grid>
+<Grid item xs={12}>
+<TextField
+           fullWidth
+           variant="outlined"
+           label="Referred By"
+           name="referredBy"
+           value={formData.referredBy}
+           onChange={handleChange}
+         />
+</Grid>
+<Grid item xs={12}>
+<TextField
+           fullWidth
+           variant="outlined"
+           label="Contact Number"
+           name="contactNumber"
+           value={formData.contactNumber}
+           onChange={handleChange}
+         />
+</Grid>
+<Grid item xs={12}>
+<TextField
+           fullWidth
+           variant="outlined"
+           label="Email"
+           name="email"
+           value={formData.email}
+           onChange={handleChange}
+         />
+</Grid>
+<Grid item xs={12}>
+<Button type="submit" variant="contained" color="primary">
+Submit
+</Button>
+</Grid>
+</Grid>
+</form>
+</Container>
+);
+};
+
+export default PatientReferralForm;
